1115b3e79fef96f523940e0ea886a3b0c12fa08c
Karl Emil Holch Andersen <karlemilandersen@gmail.com>
2024-06-16 20:57:34 +0200

    [(main)] update angular 18

--- angular-14-client/src/app/app.module.ts
+++ angular-14-client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,26 +15,19 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    RulesComponent,
-    OrderByPipe,
-    OverviewComponent,
-    UserListComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    MatTableModule,
-    MatSortModule,
-    MatIconModule,
-    MatCardModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        RulesComponent,
+        OrderByPipe,
+        OverviewComponent,
+        UserListComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MatTableModule,
+        MatSortModule,
+        MatIconModule,
+        MatCardModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
