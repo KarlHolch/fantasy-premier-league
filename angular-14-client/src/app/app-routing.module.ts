64f048159f08021d1e10385d65a2d15bf9f68829
Karl Emil Holch Andersen <karlemilandersen@gmail.com>
2024-06-16 21:28:49 +0200

    [(main)] Now compiles with angular 18

--- angular-14-client/src/app/app-routing.module.ts
+++ angular-14-client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RulesComponent } from './components/app-rules/app-rules.component';
 import { OverviewComponent } from './components/overview/overview.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'overview', pathMatch: 'full' },
   { path: 'overview', component: OverviewComponent },
+  { path: 'user-list', component: UserListComponent },
   { path: 'rules', component: RulesComponent },
 ];
 
